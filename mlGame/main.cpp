170462c6972a09fb9278b0a4f044afe992d21a7b
cookieisgood <alan.yuan.jly@gmail.com>
2018-03-24 09:24:49 -0400

    Initial Commit

---
+++ mlGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mlGame
+//
+//  Created by Alan Yuan on 2018-03-24.
+//  Copyright © 2018 Alan Yuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
